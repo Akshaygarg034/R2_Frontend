851482de6eb0b53f463091d0fbab9ba912142a08
Akshay <20bcs59@nith.ac.in>
2023-07-08 20:19:02 +0530

    minor bugs resolved

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import './App.css';
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-import React, { useState } from 'react';
 import Navbar from './components/Navbar';
 import Home from './components/Home';
 import NoteState from './context/notes/NoteState';
